39cf03638a3c8ae3200990c19f31e720b21f5f07
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-21 01:25:36 -0700

    maxchars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    for (let char of str){
+        if(!chars[char]){
+            chars[char]=1;
+        }
+    }
+
+
+}
 
 module.exports = maxChar;
