8e8ac214765aec8466471e0243f31904b39175af
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-22 12:57:34 -0700

    capitalize v2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,14 +8,18 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    let words = [];
-    for (let word of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1));
+    let result = str[0].toUpperCase();
+    for (var i = 1; i < str.length; i++){
+        if(str[i - 1] === " "){ 
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i]
+        }
+
+        } return result;
     }
 
-   return words.join(' ');
 
-}
 
 module.exports = capitalize;
 
