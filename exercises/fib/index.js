6806ca69c0147da3b0fc542e0aaca6cfff432487
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-23 15:08:32 -0700

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    result = [0, 1];
+    for(var i = 2; i <= n; i++) {
+        const a = result[i - 1];
+        const b = result[i - 2];
+
+        result.push(a+b)
+
+    } return result[n]
+
+}
 
 module.exports = fib;
+
+
+
+// function fib(n) {
+//     result = [0, 1];
+//     for (var i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+
+//         result.push(a + b)
+
+//     } return result[n]
+
+// }
