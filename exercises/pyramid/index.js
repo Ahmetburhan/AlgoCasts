5fa837abde27a3c4e4d5d7ea079362cada292b75
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-23 14:03:48 -0700

    matrixes

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -15,17 +15,46 @@
 //       '#####'
 
 function pyramid(n, row = 0, level= "") {
-    if(row = n){
+    if(row === n){
        return;
     }
     if (level.length === 2 * n - 1) {
-        
+        console.log(level);
+        return pyramid(n, row + 1);
     }
+    const midpoint = Math.floor((2 * n - 1)/ 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = "#";
+    } else {
+        add = " ";
+    } pyramid (n, row, level + add);
 }
 
-pyramid(9)
-module.exports = pyramid;
+// function pyramid(n, row = 0, level = '') {
+//     if (row === n) {
+//         return;
+//     }
 
+//     if (level.length === 2 * n - 1) {
+//         console.log(level);
+//         return pyramid(n, row + 1);
+//     }
+
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     let add;
+//     if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//         add = '#';
+//     } else {
+//         add = ' ';
+//     }
+//     pyramid(n, row, level + add);
+// }
+
+// module.exports = pyramid;
+
+module.exports = pyramid;
+pyramid(9)
 
 // function pyramid(n) {
 
