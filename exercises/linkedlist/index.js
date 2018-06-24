046de4db10b1b8c412dd5629544cf80ae5b5278a
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-24 16:16:20 -0700

    removelast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,70 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+     
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+
+    }
+    insertFirst(data){
+       
+        this.head = new Node(data, this.head);;
+
+    }
+    size(){
+        let counter = 0;
+        let node = this.head;
+        while(node){
+            counter++
+            node = node.next;
+        }
+        return counter;
+    }
+    getFirst() {
+        return this.head;
+    }
+    getLast() {
+        if(!this.head){
+            return null;
+        }
+        let node = this.head;
+        while (node) {
+            if (!node.next){
+                return node;
+            }
+            node = node.next;
+        }
+        return counter;
+    }
+    clear(){
+    this.head = null;
+
+    }
+    removeFirst(){
+        if(!this.head){
+            return
+        }
+        this.head = this.head.next;
+
+    }
+    removeLast(){
+        if (!this.head) {
+            return
+        }
+
+    }
+
+}
+
+// const.list = new LinkedList();
+// list.head = new Node(10)
 
 module.exports = { Node, LinkedList };
