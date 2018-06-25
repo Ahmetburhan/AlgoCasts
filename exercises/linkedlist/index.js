a9182156faacd37e4211a6665612ca94718739ea
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-24 22:14:53 -0700

    infinit loop

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -131,6 +131,23 @@ class LinkedList {
 
 
     }
+    forEach(fn) {
+        let node = this.head;
+        let counter = 0;
+        while(node) {
+            fn(node, counter)
+        node= node.next;
+        counter++
+    }
+}
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while(node) {
+            yield node;
+            node = node.next;
+            
+        }
+    }
 
 
 
