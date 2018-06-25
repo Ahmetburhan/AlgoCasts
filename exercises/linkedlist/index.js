7206bc931c86c4e7ec825170ab3c6ebe61fb4f52
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-24 17:05:57 -0700

    insertedAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -58,14 +58,83 @@ class LinkedList {
     }
     removeLast(){
         if (!this.head) {
-            return
+            return;
         }
+        if(!this.head.next){
+            this.head = null;
+            return;
+        }
+        let previous = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+        previous.next = null;
+
+    }
+    insertLast(data) {
+        const last = this.getLast()
+        if(last){
+            last.next =new Node(data)
+         } else {
+             this.head =new Node(data)
+
+         }
+    }
+    getAt(index) {
+     
+        let counter = 0;
+        let node = this.head;
+        while(node){
+            if(counter === index) {
+               return node; 
+            }
+
+
+            counter++;
+            node = node.next;
+        }
+        return null;
+
+    }
+    removeAt(index) {
+     if(!this.head){
+         return;
+     }
+     if (index === 0) {
+         this.head = this.head.next;
+         return;
+     }
+     const previous = this.getAt(index -1);
+     if (!previous || !previous.next){
+         return;
+     }
+   
+     previous.next = previous.next.next;
+
+
 
     }
 
+    insertAt(data, index){
+        if(!this.head){
+            this.head = new Node(data);return
+        }
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;   
+
+
+    }
+
+
 }
 
-// const.list = new LinkedList();
-// list.head = new Node(10)
+
 
 module.exports = { Node, LinkedList };
