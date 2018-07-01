e4f949bbea3f1f3c5478bd0955690c626402a777
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-01 10:35:24 -0700

    filter blogs

--- productFilters.js
+++ productFilters.js
@@ -9,3 +9,23 @@ var products = [
 
 products.filter((product) => product.type === "fruit"
     && product.quantity > 11 && product.price <= 12)
+
+
+
+
+var post = { id: 4, title: "new Post" };
+var comments = [
+    { postID: 4, content: "awesome post" },
+    { postID: 3, content: "its finet" },
+    { postID: 4, content: "neat dude" }
+];
+
+
+
+function commentsForPost(post, comments) {
+    return comments.filter(function (comment) {
+        return comment.postID === post.id;
+    });
+}
+
+commentsForPost(post, comments)
