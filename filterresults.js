6fb2ccbdcb2f07c96eac9d97d2718772861e4243
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-01 15:23:20 -0700

    filteredresults

---
+++ filterresults.js
@@ -0,0 +1,16 @@
+
+var numbers = [1, 1, 2, 3, 4, 4];
+
+function unique(array) {
+
+    return array.reduce((result, item) => {
+        if (!result.includes(item)) {
+            result.push(item);
+        }
+        return result;
+    }, []);
+
+
+}
+
+unique(numbers)
