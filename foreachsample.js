ec2ef5440d01c31a0d45c5edbdb47d19aee802df
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-06-30 23:09:03 -0700

    mapping

---
+++ foreachsample.js
@@ -0,0 +1,18 @@
+
+var images = [
+    { height: 10, width: 30 },
+    { height: 20, width: 90 },
+    { height: 54, width: 32 }
+];
+var areas = [];
+
+//images.forEach(function(image){
+//    let area = image.height * image.width;
+//  areas.push(area)})
+
+
+images.forEach(function (image) {
+    let area = image.height * image.width;
+    areas.push(area)
+})
+console.log(areas)
---
+++ mapping.js
@@ -0,0 +1,5 @@
+var paints = [{ color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+
+let pluck = paints.map((paint) => paint.color)
+    console.log(pluck)
+
