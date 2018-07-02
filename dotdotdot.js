50a18bfa91f5ec14a7b3d7a40b660c05ec92591d
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-01 20:23:22 -0700

    items added

--- dotdotdot.js
+++ dotdotdot.js
@@ -7,3 +7,30 @@ function addNumbers(...numbers) {
 
 // ... converts input to array
 console.log(addNumbers(2, 3, 4))
+
+
+
+const defaultColors = ["red", "green"];
+const userFavoriteColors = ["orange", "yellow"];
+const fallColors = ["fire red", "fall green"];
+
+
+
+
+//option1
+//defaultColors.concat(userFavoriteColors)
+
+
+
+["blue", ...fallColors, ...defaultColors, ...userFavoriteColors];
+
+
+
+function validateShoppingList(...items) {
+    if (items.indexOf("milk") < 0) {
+        return ["milk", ...items];
+    }
+    return items;
+}
+
+validateShoppingList("oranges", "bread", "eggs")
