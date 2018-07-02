ab8e42f02586fc330b10b038cd4fddbed08d41dd
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-01 21:39:57 -0700

    rest operations

--- destructuring.js
+++ destructuring.js
@@ -25,3 +25,23 @@ name;
 name2;
 rest;
 length;
+
+
+const companies = [
+    { name: "google", location: "Mountain view" },
+    { name: "Facebook", location: "Menlo PArk" },
+    { name: "Uber", location: "San Francisco" }
+]
+
+const [{ location }] = companies;
+
+location
+
+
+const Google = {
+    locations: ["mountain view", "sf", "san jose"]
+}
+
+
+const { locations: [location, location2, ...rest] } = Google;
+location2 //sf
