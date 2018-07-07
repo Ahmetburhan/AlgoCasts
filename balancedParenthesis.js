434748c732158486b46dca4d2ca9128b2851dd12
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-07 11:46:38 -0700

    balancedparens

--- balancedParenthesis.js
+++ balancedParenthesis.js
@@ -1,16 +1,16 @@
-function balancedParens(string) {
-    return !string.split("").reduce(function (previous, char) {
-        if (previous < 0) { return previous; }
-        if (char === "(") { return ++previous; }
-        if (char === "{") { return ++previous; }
-        if (char === "[") { return ++previous; }
+// function balancedParens(string) {
+//     return !string.split("").reduce(function (previous, char) {
+//         if (previous < 0) { return previous; }
+//         if (char === "(") { return ++previous; }
+//         if (char === "{") { return ++previous; }
+//         if (char === "[") { return ++previous; }
 
-        if (char === ")") { return --previous; }
-        if (char === "}") { return --previous; }
-        if (char === "]") { return --previous; }
-        return previous;
-    }, 0);
-}
+//         if (char === ")") { return --previous; }
+//         if (char === "}") { return --previous; }
+//         if (char === "]") { return --previous; }
+//         return previous;
+//     }, 0);
+// }
 
 function balanced(string) {
     return !string.split("").reduce(function (previous, char){
@@ -20,11 +20,30 @@ function balanced(string) {
     }, 0)
 }
 
+
+function balanced(string) {
+    let stack = [];
+    for (let i = 0, l = string.length; i < l ; i++ ) {
+        if (string.charAt(i) === "(" || string.charAt(i) === "{" || string.charAt(i) === "[") {
+            stack.push(string.charAt(i));
+        } else {
+            if ((stack[stack.length - 1] === "(" && string.charAt(i) === ")" || stack[stack.length - 1] === "{" && string.charAt(i) === "}" || stack[stack.length - 1] === "[" && string.charAt(i) === "]")) {
+                
+                stack.pop();
+                
+                continue;
+                
+            } return false;
+        }
+
+    } return console.log("testing", stack), stack.length === 0;
+}
+
 // balanced("(}");
 
-// console.log("this one", balanced("()())("))
+console.log("this one", balanced("{}(){}{}["))
 
-console.log("this one", balancedParens("(){}"))
+// console.log("this one", balancedParens("(){}"))
 
 
 //! mark makes boolen
