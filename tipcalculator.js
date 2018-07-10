eadc61335a471c96a5d3553a3e57ac327dc0caed
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-10 13:45:49 -0700

    tipper

--- tipcalculator.js
+++ tipcalculator.js
@@ -6,13 +6,13 @@ bills.forEach((bill) => {
 let tip;
 if (bill < 50) {
     tip = bill * 0.2;
-    tips.push(tip)
+    tips.push(Math.round(tip))
 } else if (bill >= 50 && bill <= 200) {
     tip = bill * 0.15;
-    tips.push(tip)
+    tips.push(Math.round(tip))
 } else if (bill > 200) {
     tip = bill * 0.1;
-    tips.push(tip)
+    tips.push(Math.round(tip))
 }
 })
 
