37eee1f8a746b2f9322dbd54432cd28f4e6b753c
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-11 14:32:59 -0700

    bundle js

--- complete-javascript/5-advanced-JS/starter/script.js
+++ complete-javascript/5-advanced-JS/starter/script.js
@@ -1,20 +1,119 @@
-// function constructor
-// var john = {
-//     naem : "john",
-//     yearOfBirth: 1990,
-//     job: "teacher"
+// // function constructor
+// // var john = {
+// //     naem : "john",
+// //     yearOfBirth: 1990,
+// //     job: "teacher"
+// // }
+
+// var Person = function (name, yearOfBirth, job){
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
 // }
 
-var Person = function (name, yearOfBirth, job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    this.calculateAge = function () {
-        console.log(2016 - this.yearOfBirth);
-    }
+// Person.prototype.calculateAge = function () {
+//     console.log(2016 - this.yearOfBirth);
+// }
+
+// Person.prototype.graduated = "SFSU";
+
+// // var john = new Person ("john", 1990, "teacher")
+
+// // console.log(john)
+// // john.calculateAge();
+
+// // let jane = new Person ("jane", 1988, "designer");
+// let mark = new Person("MArk", 1998, "engineer")
+// // john.calculateAge();
+// // jane.calculateAge();
+// // mark.calculateAge();
+
+// // console.log(john.graduated);
+// // console.log(jane.graduated);
+// // console.log(mark.graduated);
+
+
+// let personProto = {
+//     calculateAge: function(){
+//         console.log(2018-this.yearOfBirth)
+//     }
+// }
+
+// let john = Object.create(personProto);
+
+// // console.log(john);
+
+// let jane = Object.create(personProto, {
+//     name: {value: "Jane"},
+//     yearOfBirth: {value: "1998"},
+//     job: {value: "designer"},
+
+// })
+
+
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a);
+// console.log(b);
+
+// var years = [2005,1913,1924,1945,1967];
+
+// function arrayCalc(arr,fn){
+//     var arrRes = [];
+//     for (var i=0; i< arr.length; i++){
+//         arrRes.push(fn(arr[i]))
+//     } return arrRes;
+// }
+// function calculateAge(el) {
+//     return 2018 - el
+    
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+
+// }
+
+// function macHeartRate(el) {
+//     if(el >= 18 && el <= 120){
+//     return Math.round(206.9 - (0.67 * el));
+//     } else {
+//     return -1;
+//     }
+// }
+
+
+// let ages = arrayCalc(years, isFullAge);
+// console.log(ages);
+
+//     let heartRate = arrayCalc(ages, macHeartRate);
+//     console.log(heartRate);
+
+
+// let births = arrayCalc(years, calculateAge);
+// console.log(births)
+
+
+
+
+function interviewQuestion(job) {
+    if (job === "designer"){
+        return function(name){
+            console.log(name + ", can you please explain what UX design is?");
+        }
+    } else if (job === "teacher") {
+        return function (name) {
+            console.log(name + ", what subject do you teach");
+        }
+    } else {
+        return function (name) {
+            console.log(name + ",Hello What do you do?");
+        }
+
+}
 }
 
-var john = new Person ("john", 1990, "teacher")
 
-console.log(john)
-// john.calculateAge();
+
+
