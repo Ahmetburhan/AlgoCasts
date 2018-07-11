6e2f1ef87a0655f6b97918da96f4b69e2d0c952d
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-10 20:38:01 -0700

    Projects added

---
+++ settimer.js
@@ -0,0 +1,11 @@
+// setTimeout(function() {
+    
+// }, 2000);
+
+
+setTimeout(function () {
+    console.log("hi there after 3sn")
+    setTimeout(function() {
+        console.log("hi there after 2.3sn") 
+    }, 2000);
+},300)
