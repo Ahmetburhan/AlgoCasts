8531c012fc081bdb2e49d0d728541c73f9d32814
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-12 21:04:32 -0700

    dodgerblue

--- complete-javascript/7-ES6/starter/script.js
+++ complete-javascript/7-ES6/starter/script.js
@@ -27,16 +27,218 @@
 
 // console.log(`${firstName} `.repeat(5));
 
-const years = [ 1990,1996,2000,2003];
+// const years = [ 1990,1996,2000,2003];
 
-var ages5 = years.map(function(el){
-    return 2016 - el;
-})
+// var ages5 = years.map(function(el){
+//     return 2016 - el;
+// })
 
-console.log(ages5);
-console.log("-----------")
-const nums = [1, 2, 3, 4];
-let reduced = nums.reduce((prev, val) => { return prev + val;
+// console.log(ages5);
+// console.log("-----------")
+// const nums = [1, 2, 3, 4];
+// let reduced = nums.reduce((prev, val) => { return prev + val;
 
-},0)
-console.log(reduced)
+// },0)
+// console.log(reduced)
+
+// const years = [ 1990,1996,2000,2003];
+
+// const ages = years.map(el => 2018 - el);
+
+// console.log(ages);
+
+// ages6 = ages.map((age,index) => {
+//     return `Ages by order ${index + 1 }: ${age}`
+// });
+// console.log(ages6);
+
+// var box5 = {
+//     color: "green",
+//     position: 1,
+//     clickMe: function() {
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function(){
+//           var str = 'This is box number'  + self.position + 'and it is ' + self.color;
+//           alert(str);
+//         })
+//     }
+
+// }
+// box5.clickMe();
+
+// const box6 = {
+//     color: "green",
+//     position: 1,
+//     clickMe: function () {
+//         document.querySelector('.green').addEventListener('click', ()=> {
+//             var str = 'This is box number ' + this.position + 'and it is ' + this.color;
+//             alert(str);
+//         })
+//     }
+
+// }
+// box6.clickMe();
+
+// const box66 = {
+//     color: "green",
+//     position: 1,
+//     clickMe: function () {
+//         document.querySelector('.green').addEventListener('click', () => {
+//             var str = 'This is box number ' + this.position + 'and it is ' + this.color;
+//             alert(str);
+//         })
+//     }
+
+// }
+// box66.clickMe();
+
+// function Person(name) {
+//     this.name = name;
+
+// }
+
+// Person.prototype.myfriends5 = function(friends) {
+//     var arr = friends.map(function (el) {
+//         return this.name + ' is friends with ' + el;
+        
+//     }.bind(this))
+//     console.log(arr);
+// }
+
+// var friends = ["john", "benjamin", "aisha"];
+
+// new Person("ali").myfriends5(friends)
+
+
+// function Person(name) {
+//     this.name = name;
+
+// }
+
+// Person.prototype.myfriends6 = function (friends) {
+//     var arr = friends.map((el) => {
+//         return this.name + ' is friends with ' + el;
+
+//     })
+//     console.log(arr);
+// }
+
+// var friends = ["john", "benjamin", "aisha"];
+
+// new Person("ali").myfriends6(friends)
+
+
+// function Person(name) {
+//     this.name = name;
+
+// }
+
+// Person.prototype.myfriends6 = function (friends) {
+//     var arr = friends.map((el) => {
+//         return `${this.name} is friends with ' + ${el}`
+
+//     })
+//     console.log(arr);
+// }
+
+// var friends = ["john", "benjamin", "aisha"];
+
+// new Person("veli").myfriends6(friends)
+
+
+//destructring
+
+//es5
+
+// var john = ['john', 26];
+// var name = john[0];
+// var age = john[1];
+
+//es6
+
+// const [name, year] = ['john', 26];
+
+// const obj = {
+//     firstName : 'john',
+//     lastName : 'smith'
+// }
+
+// const {firstName, lastName} = obj;
+// console.log(firstName);
+// console.log(lastName);
+
+// const{firstName: a, lastName: b} = obj
+// console.log(a);
+// console.log(b);
+
+
+// function calcAgeRetirment (year) {
+//     const age = new Date().getFullYear()- year;
+//     return [age, 65 - age];
+// }
+
+// const [age2,retirement] = calcAgeRetirment(1990);
+// console.log(age2);
+// console.log(retirement);
+
+
+const boxes = document.querySelectorAll(".box");
+
+// //es5
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+
+// boxesArr5.forEach(function(el){
+//     el.style.backgroundColor = 'dodgerblue';
+// })
+
+//es6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
