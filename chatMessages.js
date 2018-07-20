5f7ec2e64ea66e4d3e0ae19cf716fa284983e60d
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-07-19 19:17:08 -0700

    chat mobile

--- chatMessages.js
+++ chatMessages.js
@@ -8,20 +8,22 @@ class Chatroom {
         this.author = author;
         this.recipient = recipient;
         this.messageText = messageText;
-
+        
     }
 
     addMessage(author, recipient, messageText) {
         // Create a message with author, recipient, messageText and time
-        
+        let result = [];
+
         this.author = author;
         this.recipient = recipient;
         this.messageText = messageText;
         let oneMessage = [this.author, this.recipient, this.messageText];
         console.log(oneMessage)
+        result.push(oneMessage);
         // console.log(typeof oneMessage)
 
-        // return oneMessage;
+        return result;
        
      
     }
@@ -56,15 +58,15 @@ console.log("here is chatroom",chatroom);
 console.log("number is here", Object.keys(chatroom.getMessages()).length)
 console.log(typeof messages)
 
-console.log(messages["recipient"])
-console.log("here length",messages.length)
-console.log("here different", chatroom.getMessages.length)
+console.log("recipient", messages.recipient);
+
+console.log("this is author", messages.author)
 
 console.log(chatroom.getMessagesCountByRecipient('Alice'))
 
 console.assert(chatroom.getMessagesCountByRecipient('Alice') === "Alice");
 
 // console.assert(messages.length === 3);
-// console.assert(messages[0].author === 'Bob');
+// console.assert(messages.author[0] === 'Bob');
 
 // console.assert(chatroom.getMessagesCountByRecipient('Alice') === 2);
