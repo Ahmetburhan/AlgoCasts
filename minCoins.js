9f88da77c4a37a22239ba79ffcf0dc102c85e41c
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-08-29 18:02:23 -0700

    min coins

--- minCoins.js
+++ minCoins.js
@@ -1,22 +1,39 @@
-function minStepsToOne(n, coins) {
-    let cache = {};
-    function subtractCoins(num) {
-        if (cache[num] !== undefined) {
-            return cache[num];
-        }
-        if (num === 0) {
-            return 0;
-        }
+// function minCoinChange(n, coins) {
+//   let cache = {};
+//   function subtractCoins(num) {
+//     if(cache[num] !== undefined) {
+//       return cache[num];
+//     }
+//     if(num === 0) {
+//       return 0;
+//     }
+//     let minCoins = Infinity;
+//     for(let i = 0; i < coins.length; i++) {
+//       if((num - coins[i]) >= 0) {
+//         minCoins = Math.min(minCoins, subtractCoins(num - coins[i]));
+//       }
+//     }
+//     cache[num] = minCoins + 1;
+//     return minCoins + 1;
+//   }
+//   return subtractCoins(n);
+// }
+
+
+
+function minCoinChange(num, coins) {
+    let table = [0];
+    for (let i = 1; i <= num; i++) {
         let minCoins = Infinity;
-        for (let i = 0; i < coins.length; i++) {
-            if ((num - coins[i]) >= 0) {
-                minCoins = Math.min(minCoins, subtractCoins(num - coins[i]));
+        for (let coin = 0; coin < coins.length; coin++) {
+            let currentCoin = coins[coin];
+            if ((i - currentCoin) >= 0) {
+                minCoins = Math.min(minCoins, table[i - currentCoin])
             }
         }
-        cache[num] = minCoins + 1;
-        return minCoins + 1;
+        table.push(minCoins + 1);
     }
-    return subtractCoins(n);
+    return table[num];
 }
 
-minStepsToOne(11, [5, 8, 9, 1])
+minCoinChange(11, [5, 8, 9, 1])
